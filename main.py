3267acec055c777a59e1c266bb14942984474198
Julio <28604717+xocolado@users.noreply.github.com>
2019-05-17 10:37:56 +0200

    initial commit

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ print("Welcome to the Unit Converter. Here you can convert kilometres into miles
 
 # The program asks user to enter number of kilometers.
 # User enters the amount of kilometers
-km = float(input("Please, enter a number of kilometers you wish to convert into miles: "))
+while True:
+    km = float(input("Please, enter a number of kilometers you wish to convert into miles: "))
 
-
-conversion_factor = 0.621371
-miles = km * conversion_factor
+    conversion_factor = 0.621371
+    miles = km * conversion_factor
 
 #The program converts these kilometers into miles and prints them.
-print("The conversion of kilometers into miles is: " + str(miles))
+    print("The conversion of " + str(km) + " kilometers into miles is: " + str(miles) + " miles.")
 
 # The program asks user if s/he'd want to do another conversion.
-print("Do you want to do another conversion? Yes/No")
+    choice = input("Do you want to do another conversion? Yes/No ")
+
+    if choice.lower() != "y" and choice.lower() != "yes":
+        break
+
+
+
+
 
 
 
